9bc6a1850fd8decab95356b5e27bf34fd8d9c142
Mr-Owllers <mr.owllers@gmail.com>
2022-09-11 16:32:14 +0100

    fixed ez run

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-python=/usr/bin/python3.10
-pip=/config/.local/bin/pip3.10 # change path according to where pip/python is in your os
-
-pip install -r requirements.txt
+pip3.10 install -r requirements.txt
 clear
-python main.py
+python3.10 main.py
